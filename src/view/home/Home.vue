e56963fcf428a0018c50d589b19f71ba05865f7b
18676342166 <yanyu10!>
2018-09-26 22:48:59 +0800

    添加bootstrap插件

---
+++ src/view/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ul class="nav nav-pills">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Profile</a></li>
+            <li role="presentation"><a href="#">Messages</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Home'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
