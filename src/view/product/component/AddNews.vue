0687c25336a63863a7d76ec4b5fa0e84bd1efb73
18676342166 <yanyu10!>
2018-11-08 23:44:37 +0800

    提交

--- src/view/product/component/AddNews.vue
+++ src/view/product/component/AddNews.vue
@@ -1,44 +1,44 @@
 <template>
- <div class="components-container">
-  <div class="info">UE编辑器示例<br>需要使用编辑器时，调用UE公共组件即可。可设置填充内容defaultMsg，配置信息config(宽度和高度等)，可调用组件中获取内容的方法。</div>
-  <button @click="getUEContent()">获取内容</button>
-  <div class="editor-container">
-   <UE :defaultMsg=defaultMsg :config=config ref="ue"></UE>
-  </div>
- </div>
+    <div class="add-news">
+        <ueditor :model="content"></ueditor>
+        <button @click="check">check</button>
+    </div>
 </template>
 <script>
-import UE from '@/view/common/UE'
+import ueditor from './ueditor'
+
 export default {
-  components: { UE },
+  name: 'AddNews',
   data () {
     return {
-      defaultMsg: '这里是UE测试',
-      config: {
-        initialFrameWidth: null,
-        initialFrameHeight: 350
-      }
+      msg: 'Welcome to Your Vue.js App',
+      content: ''
     }
   },
   methods: {
-    getUEContent () {
-      let content = this.$refs.ue.getUEContent()
-      this.$notify({
-        title: '获取成功，可在控制台查看！',
-        message: content,
-        type: 'success'
-      })
-      console.log(content)
+    check () {
+      alert(this.content)
     }
-  }
+  },
+  components: {ueditor}
 }
 </script>
-<style>
-.info {
-  border-radius: 10px;
-  line-height: 20px;
-  padding: 10px;
-  margin: 10px;
-  background-color: #ffffff;
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
