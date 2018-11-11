5a52ea482e63618ce66376eaeff7d42c50daf692
18676342166 <yanyu10!>
2018-11-12 00:00:10 +0800

    提交

--- static/UE/ueditor.config.js
+++ static/UE/ueditor.config.js
@@ -32,8 +32,8 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        // , serverUrl: URL + "jsp/controller.jsp"
-        , serverUrl: "http://www.gugualao.top/file/upload"
+        , serverUrl: URL + "jsp/controller.jsp"
+        // , serverUrl: "http://www.gugualao.top/file/upload"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         , toolbars: [[
