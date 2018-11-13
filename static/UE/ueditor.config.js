018056ec03afc153c2c0b4af39247e183e968f4e
18676342166 <yanyu10!>
2018-11-13 23:46:24 +0800

    提交

--- static/UE/ueditor.config.js
+++ static/UE/ueditor.config.js
@@ -32,7 +32,7 @@
 
         // 服务器统一请求接口路径
         // , serverUrl: "http://gugualao.top/file/upload/"
-        , serverUrl: "http://localhost:9002/file/upload/"
+        , serverUrl: "http://localhost:9002/ueditor/exec/"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
